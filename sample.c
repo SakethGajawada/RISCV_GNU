d6abe0a9cb3579da4d32ff1911f61baa348a7f32
SakethGajawada <77500278+SakethGajawada@users.noreply.github.com>
2023-08-21 23:25:31 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,23 @@
+void newlogic(int input1, int input2,int input3, int* output1, int* output2)
+{
+    *output1 = (input1&input2)|input3;
+    *output2 = input1|input2;
+}
+
+void read()
+{
+    int input1=0;
+    int input2=0;
+    int input3=0;
+    int output1;
+    int output2; 
+    newlogic(input1,input2,input3,&output1,&output2); 
+
+    // printf("output1 = %d", output1); 
+    // printf("output2 = %d", output2); 
+}
+
+int main()
+{
+	read();
+}
